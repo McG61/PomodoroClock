27bf5ecf679cfe7e85defc31bf356ae2849d7c42
HDU-blank <2583066474@qq.com>
2017-08-08 20:28:34 +0800

    modify

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ $(function(){
         var seconds=parseInt(time%60);
         if (seconds<10){seconds="0"+seconds};
         lefttime=minutes+":"+seconds;
-        $(".progress_time").html(lefttime);
+        $(".progress-time").html(lefttime);
         time--;
         if(status){
             mask_height=(1-time/(workTime*60))*300;
@@ -28,9 +28,9 @@ $(function(){
             status=!status;
             if(status){
                 time=workTime*60;
-                $("#bgMusic")[0].pause();
-                $("#bgMusic")[0].currentTime = 0;
-                $(".progress_text").html("Work");
+                $("#bg-music")[0].pause();
+                $("#bg-music")[0].currentTime = 0;
+                $(".progress-text").html("Work");
                 $(".mask").css({
                     "height":"0px",
                     "background-color":"#99CC00"
@@ -41,9 +41,9 @@ $(function(){
                 time=restTime*60;
                 bgm_num=parseInt(4*Math.random());
                 console.log(bgm_num);
-                $("#bgMusic").attr('src','audio/'+bgm_num+'.mp3');
-                $("#bgMusic")[0].play();
-                $(".progress_text").html("Rest");
+                $("#bg-music").attr('src','audio/'+bgm_num+'.mp3');
+                $("#bg-music")[0].play();
+                $(".progress-text").html("Rest");
                 $(".mask").css({
                     "height":"0px",
                     "background-color":"#F71E35"
@@ -54,53 +54,53 @@ $(function(){
             int=setInterval(clock, 1000);
         }
     }
-    $(".control_rest .btn_subtract").click(function(event) {  //rest-按钮功能
+    $(".control-rest .btn-subtract").click(function(event) {  //rest-按钮功能
         restTime=$(this).next().html();
         if(restTime>1)
         restTime--;
         $(this).next().html(restTime);
         if (!status){
-            $(".progress_time").html(restTime);
+            $(".progress-time").html(restTime);
             time=restTime*60;
         }
     });
-    $(".control_rest .btn_add").click(function(event) {   //rest+按钮功能
+    $(".control-rest .btn-add").click(function(event) {   //rest+按钮功能
         restTime=$(this).prev().html();
         restTime++;
         $(this).prev().html(parseInt(restTime));
         if (!status){
-            $(".progress_time").html(restTime);
+            $(".progress-time").html(restTime);
             time=restTime*60;
         }
-        //$(".control_rest button").attr("disabled", true);
+        //$(".control-rest button").attr("disabled", true);
     }); 
-    $(".control_work .btn_subtract").click(function(event) {  //work-按钮功能
+    $(".control-work .btn-subtract").click(function(event) {  //work-按钮功能
         workTime=$(this).next().html();
         if(workTime>1)
         workTime--;
         $(this).next().html(workTime);
         if (status){
-            $(".progress_time").html(workTime);
+            $(".progress-time").html(workTime);
             time=workTime*60;
         }
     });
-    $(".control_work .btn_add").click(function(event) {   //work+按钮功能
+    $(".control-work .btn-add").click(function(event) {   //work+按钮功能
         workTime=$(this).prev().html();
         workTime++;
         $(this).prev().html(parseInt(workTime));
         if (status){
-            $(".progress_time").html(workTime);
+            $(".progress-time").html(workTime);
             time=workTime*60;
         }
-        //$(".control_rest button").attr("disabled", true);
+        //$(".control-rest button").attr("disabled", true);
     }); 
     $(".progress").click(function(event) {
         if (!flag){
             clearInterval(int);
             if (!status){
-                $(".control_rest button").attr("disabled", false);
+                $(".control-rest button").attr("disabled", false);
             }else{
-                $(".control_work button").attr("disabled", false);
+                $(".control-work button").attr("disabled", false);
             }
             flag=!flag;
         }
