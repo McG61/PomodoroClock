007a6d12e984f440db4afe2d9104f771cf9cdc59
HDU-blank <2583066474@qq.com>
2017-09-20 10:37:19 +0800

    modify

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 $(function(){
     var int=setInterval(clock, 1000);
     clearInterval(int);
-    var bgm_num=0;
+    var bgm_num=0;      //Music serial number
     var mask_height=0;
     var workTime=25;
     var restTime=5;
-    var flag=true;
-    var status=true;
-    var time=1500;
+    var flag=true;      //play or pause
+    var status=true;    //work or rest status
+    var time=1500;      //workTime*60 or restTime*60 (minutes To seconds)
+    /*计时函数*/
     function clock(){
         var hours=Math.floor((time)/3600);
         if (hours<10){hours="0"+hours};
@@ -40,8 +41,17 @@ $(function(){
             else if(!status){
                 time=restTime*60;
                 bgm_num=parseInt(4*Math.random());
-                console.log(bgm_num);
+                //console.log(bgm_num);
                 $("#bg-music").attr('src','audio/'+bgm_num+'.mp3');
+                $("#bg-music")[0].addEventListener('ended', function () {  
+                    setTimeout(function () {
+                        if(!status){ 
+                            bgm_num=parseInt(4*Math.random());
+                            $("#bg-music").attr('src','audio/'+bgm_num+'.mp3');
+                            $("#bg-music")[0].play(); 
+                        }
+                    }, 500);  
+                }, false);
                 $("#bg-music")[0].play();
                 $(".progress-text").html("Rest");
                 $(".mask").css({
@@ -99,6 +109,7 @@ $(function(){
             clearInterval(int);
             if (!status){
                 $(".control-rest button").attr("disabled", false);
+                $("#bg-music")[0].pause();
             }else{
                 $(".control-work button").attr("disabled", false);
             }
@@ -108,6 +119,9 @@ $(function(){
             clearInterval(int);
             int=setInterval(clock, 1000);
             $(".control button").attr("disabled", true);
+            if (!status && $("#bg-music")[0].paused){
+                $("#bg-music")[0].play();
+            }
             flag=!flag;
         }
     });
